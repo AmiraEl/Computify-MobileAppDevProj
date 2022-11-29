3dd5ac840f0f38b566ddf919599ad8a9a0babcf7
Amira <amira.elnashar13516@gmail.com>
2022-11-30 00:51:35 +0400

    add listing

--- app/src/main/java/com/example/myapplication/AddEditActivity.java
+++ app/src/main/java/com/example/myapplication/AddEditActivity.java
@@ -9,9 +9,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
@@ -19,6 +20,8 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
+
 
 import java.util.HashMap;
 import java.util.List;
@@ -36,7 +39,6 @@ public class AddEditActivity extends AppCompatActivity implements View.OnClickLi
 //    private TextView hddTV;
 //    private TextView ssdTV;
     private TextView titleTV;
-
     private EditText nameET;
     private EditText gpuET;
     private EditText cpuET;
@@ -46,17 +48,10 @@ public class AddEditActivity extends AppCompatActivity implements View.OnClickLi
     private EditText psuET;
     private EditText hddET;
     private EditText ssdET;
-
     private Button addButton;
     private Button cancelButton;
-
     static FirebaseFirestore db;
-
     private int pcID;
-
-    private FirebaseAuth auth;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,9 +75,6 @@ public class AddEditActivity extends AppCompatActivity implements View.OnClickLi
 
         addButton.setOnClickListener(this);
         cancelButton.setOnClickListener(this);
-
-
-
         db = FirebaseFirestore.getInstance();
 
 
@@ -93,45 +85,34 @@ public class AddEditActivity extends AppCompatActivity implements View.OnClickLi
         if(v.getId() == R.id.buttonSAVE){
 
             //ADD/EDIT TO DATABASE
-
             CollectionReference computers = db.collection("computers");
+            Map<String, Object> computer = new HashMap<>();
+            computer.put("pcID", 1);
+            computer.put("UID", LoginActivity.profile.getUID());
+            computer.put("name", nameET.getText().toString());
+            computer.put("gpu", gpuET.getText().toString());
+            computer.put("cpu", cpuET.getText().toString());
+            computer.put("ram", ramET.getText().toString());
+            computer.put("case", caseET.getText().toString());
+            computer.put("mother", motherET.getText().toString());
+            computer.put("psu", psuET.getText().toString());
+            computer.put("hdd", hddET.getText().toString());
+            computer.put("ssd", ssdET.getText().toString());
 
-
-
-            computers.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//            computers.document(Integer.toString(1)).set(computer);
+            computers.add(computer).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                 @Override
-                public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                    if(task.isSuccessful()){
-                     List<String> o = (List<String>) task.getResult().getDocuments().get(task.getResult().size() -1).get("pcID");
-                      pcID = Integer.parseInt(o.get(o.size()-1));
-                      pcID++;
+                public void onSuccess(DocumentReference documentReference) {
+                    Log.d("TAG", "DocumentSnapshot written with ID: " + documentReference.getId());
 
-                        Toast.makeText(getApplicationContext(), Integer.toString(pcID), Toast.LENGTH_LONG).show();
-                        Log.d("ABOOK", Integer.toString(pcID));
-
-
-                        Map<String, Object> computer = new HashMap<>();
-                        computer.put("pcID", pcID);
-                        computer.put("UID", LoginActivity.profile.getUID());
-                        computer.put("name", nameET.getText().toString());
-                        computer.put("gpu", gpuET.getText().toString());
-                        computer.put("cpu", cpuET.getText().toString());
-                        computer.put("ram", ramET.getText().toString());
-                        computer.put("case", caseET.getText().toString());
-                        computer.put("mother", motherET.getText().toString());
-                        computer.put("psu", psuET.getText().toString());
-                        computer.put("hdd", hddET.getText().toString());
-                        computer.put("ssd", ssdET.getText().toString());
-
-                        computers.document(Integer.toString(pcID)).set(computer);
-
-                    }
                 }
-            });
-
-
-
-
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w("TAG", "Error adding document", e);
+                }
+            })
+            ;
 
         }
         else if(v.getId() == R.id.buttonCANCEL){
