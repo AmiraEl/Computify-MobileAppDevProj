c8c2d27b0f031431d9f96da37ec8c00ca3e655c1
YusufShan <yusufboss2@gmail.com>
2022-11-29 20:58:23 +0400

    AddEdit

--- app/src/main/java/com/example/myapplication/AddEditActivity.java
+++ app/src/main/java/com/example/myapplication/AddEditActivity.java
@@ -3,8 +3,36 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AddEditActivity extends AppCompatActivity {
+
+    private TextView nameTV;
+    private TextView gpuTV;
+    private TextView cpuTV;
+    private TextView ramTV;
+    private TextView caseTV;
+    private TextView motherTV;
+    private TextView psuTV;
+    private TextView hddTV;
+    private TextView ssdTV;
+
+    private EditText nameET;
+    private EditText gpuET;
+    private EditText cpuET;
+    private EditText ramET;
+    private EditText caseET;
+    private EditText motherET;
+    private EditText psuET;
+    private EditText hddET;
+    private EditText ssdET;
+
+    private Button addButton;
+    private Button cancelButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
