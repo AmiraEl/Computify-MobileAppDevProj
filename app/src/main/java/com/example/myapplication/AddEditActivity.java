79b76670d18b4e936062e173961604ac428be4ed
Amira <amira.elnashar13516@gmail.com>
2022-11-29 21:13:38 +0400

    Add edit xml

--- app/src/main/java/com/example/myapplication/AddEditActivity.java
+++ app/src/main/java/com/example/myapplication/AddEditActivity.java
@@ -9,15 +9,16 @@ import android.widget.TextView;
 
 public class AddEditActivity extends AppCompatActivity {
 
-    private TextView nameTV;
-    private TextView gpuTV;
-    private TextView cpuTV;
-    private TextView ramTV;
-    private TextView caseTV;
-    private TextView motherTV;
-    private TextView psuTV;
-    private TextView hddTV;
-    private TextView ssdTV;
+//    private TextView nameTV;
+//    private TextView gpuTV;
+//    private TextView cpuTV;
+//    private TextView ramTV;
+//    private TextView caseTV;
+//    private TextView motherTV;
+//    private TextView psuTV;
+//    private TextView hddTV;
+//    private TextView ssdTV;
+    private TextView titleTV;
 
     private EditText nameET;
     private EditText gpuET;
@@ -33,10 +34,21 @@ public class AddEditActivity extends AppCompatActivity {
     private Button cancelButton;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit);
+
+        titleTV = findViewById(R.id.textViewMain);
+
+        nameET = findViewById(R.id.);
+        gpuET = findViewById(R.id.);
+        cpuET = findViewById(R.id.);
+        ramET = findViewById(R.id.);
+        caseET = findViewById(R.id.);
+        motherET = findViewById(R.id.);
+        psuET = findViewById(R.id.);
+        hddET = findViewById(R.id.);
+        ssdET = findViewById(R.id.);
     }
 }
