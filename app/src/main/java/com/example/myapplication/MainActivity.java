8f790eaaa6a9cceb9853e87e805e7af2c2e5c314
YusufShan <yusufboss2@gmail.com>
2022-11-30 16:22:02 +0400

    MENUUUUUUUUUUUUUUUUUUUUUUUUUUUUUu

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -12,6 +16,7 @@ import android.widget.SimpleAdapter;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -77,8 +82,75 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
 
+    
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        super.onCreateOptionsMenu(menu);
+
+        menu.getItem(0).setVisible(false);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+//        if(item.getItemId() == R.id.home){
+//
+//            Intent HomeIntent = new Intent(MainActivity.this, MainActivity.class);
+//            startActivity(HomeIntent);
+//
+//        }
+        if(item.getItemId() == R.id.addlisting){
+            Intent AddIntent = new Intent(MainActivity.this, AddEditActivity.class);
+
+            startActivity(AddIntent);
+        }
+        if(item.getItemId() == R.id.viewlisting){
+
+            Intent ViewIntent = new Intent(MainActivity.this, MainActivity.class);
+
+            ViewIntent.putExtra("View", LoginActivity.profile.getUID());
+
+            startActivity(ViewIntent);
+
+        }
+        if(item.getItemId() == R.id.profile){
+
+            //TBA WHEN THE PROFILE ACTIVITY IS CREATED
+
+            Intent ProfileIntent = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(ProfileIntent);
+
+        }
+        if(item.getItemId() == R.id.about){
+
+
+            //TBA WHEN ABOUT ACTIVITY IS CREATED
+            Intent HomeIntent = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(HomeIntent);
+
+        }
+        if(item.getItemId() == R.id.logout){
+
+            FirebaseAuth.getInstance().signOut();
+
+            Intent HomeIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(HomeIntent);
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
